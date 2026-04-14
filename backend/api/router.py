85fa8be11c4e228906c515fc7b174807683b466d
yusufashrafiya29-eng <yusufashrafiya29@gmail.com>
2026-04-15 04:59:49 +0530

    fixed import paths

--- backend/api/router.py
+++ backend/api/router.py
@@ -1,6 +1,14 @@
 from fastapi import APIRouter
-from api.api_v1.router import api_router as api_v1_router
 
 main_router = APIRouter()
 
-main_router.include_router(api_v1_router, prefix="/api/v1")
+@main_router.get("/test")
+def test_api():
+    return {"message": "API working 🔥"}
+
+# from fastapi import APIRouter
+# from api.api_v1.router import api_router as api_v1_router
+
+# main_router = APIRouter()
+
+# main_router.include_router(api_v1_router, prefix="/api/v1")
